aec4f39d89c22b8c2c4e57496ad38ae010e540c0
1322harshd <harsh.nz@outlook.com>
2026-06-06 22:17:26 +1200

    Refactor code structure for improved readability and maintainability

--- jira_clone/src/pages/login.jsx
+++ jira_clone/src/pages/login.jsx
@@ -1,9 +1,48 @@
 import logo from '../assets/logos/logo.png'; 
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Profile(){
+const [form, setForm] = useState({
+    email:'',
+    password:''
+})
+
+
+const handleChange = (e) =>{
+    setForm({...form,[e.target.name]:e.target.value});
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+        const response = await axios.get('https://localhost:3002/login',form);
+        console.log(response);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
     return(
         <>
-        <img src={logo} alt="website-logo" />
+        <section className='header'>    
+        <img src={logo} alt="website-logo" width="200px" height="100px" />
+        </section>
+        <section className='login-inputs'>
+            <form onSubmit={handleSubmit}>
+
+                <label htmlFor="email">Email:</label>
+                <input id="email" name="email" type="email" value={form.email} onChange={handleChange}></input>
+
+                <label htmlFor="password">Password:</label>
+                <input id="password" name="password" type="password" value={form.password} onChange={handleChange}></input>
+                
+                <button type="submit" className="btn" >
+                        Login
+                    </button>
+            </form>
+        </section>
+
         </>
     )
 }
