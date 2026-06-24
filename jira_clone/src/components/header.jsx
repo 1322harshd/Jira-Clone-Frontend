c519a82cc10605e1b7d546602953ccb9829cd5e9
1322harshd <harsh.nz@outlook.com>
2026-06-24 20:58:00 +1200

    Add UserIcon component with user image display and styling

--- jira_clone/src/components/header.jsx
+++ jira_clone/src/components/header.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/logos/logo.png'; 
+import UserIcon from './usericon';
 import { useState } from 'react';
 import './header.css'
 
@@ -8,10 +9,12 @@ export default function Header(){
         <>
        <section className='header'>
        <img src={logo} alt="website-logo" width="200px" height="100px" padding-left="20px" />
- 
- <input 
-        </section>
+       
 
+       <input type="text" placeholder='Search' name="search" ></input>
+
+       <UserIcon />
+       </section>
         </>
     )
 }
---
+++ jira_clone/src/components/usericon.jsx
@@ -0,0 +1,14 @@
+import image from '../assets/images/images.jpeg'
+export default function UserIcon(){
+
+
+
+return (
+    <>
+    <div className="main-icon">
+        <img src={image} alt="user-image" className='user-image'></img>
+    </div>
+    </>
+)
+
+}
